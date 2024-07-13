713b3896a3508b249cd0a2e123fa1d6022754539
shalluv <leon555thai@gmail.com>
2024-07-13 20:22:56 +0700

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
-import "@/styles/globals.css";
-
 import { Anuphan } from "next/font/google";
+import { TRPCReactProvider } from "@/trpc/react";
+import { cn } from "@/lib/utils";
+
+import "@/styles/globals.css";
 
 export const anuphan = Anuphan({
     subsets: ["latin"],
@@ -8,10 +10,6 @@ export const anuphan = Anuphan({
     weight: ["200", "300", "400", "500", "600", "700"],
     display: "swap",
 });
-
-import { TRPCReactProvider } from "@/trpc/react";
-import { cn } from "@/lib/utils";
-import ESCLogoWithoutText from "@/components/esc/ESCLogoWithoutText";
 
 export const metadata = {
     title: "Intania OIDC",
@@ -32,14 +30,6 @@ export default function RootLayout({
                 )}
             >
                 <TRPCReactProvider>{children}</TRPCReactProvider>
-                <div className="pointer-events-none relative -z-50 h-dvh w-dvw select-none overflow-hidden">
-                    <ESCLogoWithoutText
-                        className="absolute -bottom-[30dvh] -right-[10dvw] -z-50 w-[60dvw] rotate-[-12deg]"
-                        fill={"hsla(0, 0%, 0%, 0.03)"}
-                    />
-                    <div className="absolute left-0 top-0 -z-50 size-[150dvw] -translate-x-1/2 -translate-y-2/3 bg-background-gradient-prop opacity-20" />
-                    <div className="absolute left-0 top-0 -z-50 size-[80dvw] -translate-x-2/3 -translate-y-[90%] bg-background-gradient-prop" />
-                </div>
             </body>
         </html>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
+import ESCLogoWithoutText from "@/components/esc/ESCLogoWithoutText";
 import { Footer } from "@/components/footer";
 import { UserBox } from "@/components/user-box";
 
 export default async function Home() {
     return (
-        <div className="flex size-full flex-col items-center">
-            <section className="absolute top-1/2 flex size-full max-w-3xl -translate-y-1/2 flex-col items-center justify-between md:h-auto md:justify-center md:px-32 lg:max-w-6xl">
-                <UserBox />
-                <Footer />
-            </section>
-        </div>
+        <>
+            <div className="flex size-full flex-col items-center">
+                <section className="absolute top-1/2 flex size-full max-w-3xl -translate-y-1/2 flex-col items-center justify-between md:h-auto md:justify-center md:px-32 lg:max-w-6xl">
+                    <UserBox />
+                    <Footer />
+                </section>
+            </div>
+            <div className="pointer-events-none relative -z-50 h-dvh w-dvw select-none overflow-hidden">
+                <ESCLogoWithoutText
+                    className="absolute -bottom-[30dvh] -right-[10dvw] -z-50 w-[60dvw] rotate-[-12deg]"
+                    fill={"hsla(0, 0%, 0%, 0.03)"}
+                />
+                <div className="absolute left-0 top-0 -z-50 size-[150dvw] -translate-x-1/2 -translate-y-2/3 bg-background-gradient-prop opacity-20" />
+                <div className="absolute left-0 top-0 -z-50 size-[80dvw] -translate-x-2/3 -translate-y-[90%] bg-background-gradient-prop" />
+            </div>
+        </>
     );
 }
