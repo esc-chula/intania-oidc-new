68ce908681d82d57465c7697ef4b4550e039a8c9
ebonian <tanadon.santisan@gmail.com>
2024-07-15 05:45:23 +0700

    chore: protected register/onboarding

--- src/app/register/onboarding/layout.tsx
+++ src/app/register/onboarding/layout.tsx
@@ -1,10 +1,17 @@
-"use client";
-
 import { UserFormContextProvider } from "@/contexts/form-context";
 import Progress from "./_components/progress";
 import Transition from "./template";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+    const cookieStore = cookies();
+    const sid = cookieStore.get("sid");
+
+    if (!sid) {
+        redirect("/");
+    }
+
     return (
         <article className="flex size-full flex-col gap-24 px-6 py-7">
             <UserFormContextProvider>
