a75cc47d6a9e1826c0bc7295f600e6216bc43f56
tntons <saharat.nuamchit@gmail.com>
2024-07-15 05:06:40 +0700

    chore: sleepy

--- src/app/register/onboarding/step-two/page.tsx
+++ src/app/register/onboarding/step-two/page.tsx
@@ -7,6 +7,13 @@ import {
     FormLabel,
     FormMessage,
 } from "@/components/ui/form";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { useUserForm } from "@/contexts/form-context";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -14,7 +21,37 @@ import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import { useEffect } from "react";
+import { useEffect , useState } from "react";
+import GoogleMap from "@/components/maps/maps";
+import { fetchData } from "./action";
+
+interface Country {
+  code: string | null;
+  name: string | null;
+  id: number;
+  createdAt: Date;
+  updatedAt: Date | null;
+}
+
+interface Province {
+    id: number;
+    createdAt: Date;
+    updatedAt: Date | null;
+    nameTh: string | null;
+    nameEn: string | null;
+    provinceCode: number | null; // Allow provinceCode to be number or null
+};
+
+interface District {
+    id: number;
+    createdAt: Date;
+    updatedAt: Date | null;
+    nameTh: string | null;
+    nameEn: string | null;
+    provinceCode: number | null;
+    districtCode: number | null;
+    postalCode: number | null;
+}
 
 export default function Page() {
     const formContext = useUserForm();
@@ -47,10 +84,42 @@ export default function Page() {
         mode: "onChange",
     });
 
+
+
     function onSubmit(values: z.infer<typeof formSchema>) {
         formContext.updateUserData(values);
         router.push("/register/onboarding/step-three");
     }
+
+    const handleLocationSelect = (lat: string, lng: string) => {
+        form.setValue("currentAddressLatitude", lat);
+        form.setValue("currentAddressLongitude", lng);
+        console.log(`Selected location: Latitude: ${lat}, Longitude: ${lng}`);
+    };
+
+    const [countries, setCountries] = useState<Country[]>([]);
+    const [provinces, setProvinces] = useState<Province[]>([]);
+    const [districts, setDistricts] = useState<District[]>([]);
+    const [selectedCountry, setSelectedCountry] = useState('');
+    const [selectedProvince, setSelectedProvince] = useState(0);
+    console.log(countries);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const data = await fetchData();
+                console.log(data);
+                if(data != null){
+                    setCountries(data.countries);
+                    setProvinces(data.thaiProvinces);
+                    setDistricts(data.thaiDistricts);
+                }
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        })();
+    }, []);
+
 
     useEffect(() => {
         formContext.setStep(2);
@@ -179,7 +248,252 @@ export default function Page() {
                         )}
                     />
                 </section>
-                {/* TODO: MAP */}
+
+                <section className="flex flex-col gap-2">
+                    <FormField
+                        control={form.control}
+                        name="currentAddressLatitude"
+                        render={({ field }) => (
+                            <Input type="hidden" {...field} />
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="currentAddressLongitude"
+                        render={({ field }) => (
+                            <Input type="hidden" {...field} />
+                        )}
+                    />
+                    <div className="rounded-lg border-[6px] border-white overflow-hidden h-60">
+                        <GoogleMap onLocationSelect={handleLocationSelect} width="100%" height="100%" placeholder="ระบุตำแหน่งที่อยู่ปัจจุบัน"/>
+                    </div>
+
+                    <FormField
+                        control={form.control}
+                        name="currentAddressProvinceId"
+                        render={({ field }) => (
+                            <FormItem className="!pt-0">
+                                <FormLabel>จังหวัดที่อยู่ปัจจุบัน</FormLabel>
+                                <Select
+                                    onValueChange={(value) => {
+                                        const selectedProvinceString = provinces.find(province => province.nameTh === value);
+                                        field.onChange(selectedProvinceString?.provinceCode);
+                                        setSelectedProvince(selectedProvinceString?.provinceCode);
+                                        // Additional logic to update districts based on selected province
+                                    }}
+                                >
+                                    <FormControl>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="เลือกจังหวัดที่อยู่ปัจจุบัน" />
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        {/* Map through provinces data */}
+                                        {provinces.map((province) => (
+                                            <SelectItem key={province.provinceCode} value={province.nameTh}>
+                                                {province.nameTh}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="currentAddressDistrictId"
+                        render={({ field }) => (
+                            <FormItem className="!pt-0">
+                                <FormLabel>เขตที่อยู่ปัจจุบัน</FormLabel>
+                                <Select
+                                    onValueChange={(value) => {
+                                        const selectedDistrict = districts.find(district => district.nameTh === value);
+                                        field.onChange(selectedDistrict?.districtCode);
+                                        // Additional logic to update districts based on selected province
+                                    }}
+                                >
+                                    <FormControl>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="เลือกเขตที่อยู่ปัจจุบัน" />
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        {districts.filter(district => {
+                                            console.log('Filtering Districts for Province Code:', selectedProvince); // Debugging
+                                            return district.provinceCode === selectedProvince;
+                                        }).map((district) => (
+                                            <SelectItem key={district.districtCode} value={district.nameTh}>
+                                                {district.nameTh}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+
+
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="currentAddressOther"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>ที่อยู่ปัจจุบัน</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="กรอกที่อยู่ปัจจุบัน"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                   
+                </section>
+
+                <section className="flex flex-col gap-2">
+                    <FormField
+                        control={form.control}
+                        name="hometownAddressLatitude"
+                        render={({ field }) => (
+                            <Input type="hidden" {...field} />
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="hometownAddressLongitude"
+                        render={({ field }) => (
+                            <Input type="hidden" {...field} />
+                        )}
+                    />
+                    <div className="rounded-lg border-[6px] border-white overflow-hidden h-60">
+                        <GoogleMap onLocationSelect={handleLocationSelect} width="100%" height="100%" placeholder="ระบุตำแหน่งที่อยู่ภูมิลำเนา" />
+                    </div>
+
+                    <FormField
+                        control={form.control}
+                        name="nationalityId"
+                        render={({ field }) => (
+                            <FormItem className="!pt-0">
+                                <FormLabel>ประเทศที่อยู่ภูมิลำเนา</FormLabel>
+                                <Select
+                                    onValueChange={(value) => {
+                                        console.log(value);
+                                        const selectedCountry = countries.find(country => country.id === value);
+                                        field.onChange(selectedCountry?.id);
+                                    }}
+                                >
+                                    <FormControl>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="เลือกประเทศที่อยู่ภูมิลำเนา" />
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        {/* Map through nationalities data */}
+                                        {countries.map((nationality, index) => (
+                                            <SelectItem key={index} value={nationality.name}>
+                                                {nationality.name}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="hometownAddressProvinceId"
+                        render={({ field }) => (
+                            <FormItem className="!pt-0">
+                                <FormLabel>จังหวัดที่อยู่ภูมิลำเนา</FormLabel>
+                                <Select
+                                    onValueChange={(value) => {
+                                        const selectedProvinceString = provinces.find(province => province.nameTh === value);
+                                        field.onChange(selectedProvinceString?.provinceCode);
+                                        setSelectedProvince(selectedProvinceString?.provinceCode);
+                                        // Additional logic to update districts based on selected province
+                                    }}
+                                >
+                                    <FormControl>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="เลือกจังหวัดที่อยู่ภูมิลำเนา" />
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        {/* Map through provinces data */}
+                                        {provinces.map((province) => (
+                                            <SelectItem key={province.provinceCode} value={province.nameTh}>
+                                                {province.nameTh}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="hometownAddressDistrictId"
+                        render={({ field }) => (
+                            <FormItem className="!pt-0">
+                                <FormLabel>เขตที่อยู่ภูมิลำเนา</FormLabel>
+                                <Select
+                                    onValueChange={(value) => {
+                                        const selectedDistrict = districts.find(district => district.nameTh === value);
+                                        field.onChange(selectedDistrict?.districtCode);
+                                        // Additional logic to update districts based on selected province
+                                    }}
+                                >
+                                    <FormControl>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="เลือกเขตที่อยู่ภูมิลำเนา" />
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        {districts.filter(district => {
+                                            console.log('Filtering Districts for Province Code:', selectedProvince); // Debugging
+                                            return district.provinceCode === selectedProvince;
+                                        }).map((district) => (
+                                            <SelectItem key={district.districtCode} value={district.nameTh}>
+                                                {district.nameTh}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+
+
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="hometownAddressOther"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>ที่อยู่ภูมิลำเนา</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="กรอกที่อยู่ภูมิลำเนา"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                </section>
                 <Button type="submit" className="self-end" size="lg">
                     ถัดไป
                 </Button>
--- src/components/maps/maps.tsx
+++ src/components/maps/maps.tsx
@@ -1,11 +1,6 @@
 import React, { useState } from 'react';
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 
-const containerStyle = {
-    width: '400px',
-    height: '400px'
-};
-
 const mapOptions = {
     center: {
         lat: 13.73826,
@@ -20,8 +15,16 @@ interface Location{
     lng: number | null;
 };
 
-export default function GoogleMaps() {
+interface GoogleMapsProps {
+    onLocationSelect: (lat: number, lng: number) => void;
+    width?: string;
+    height?: string;
+    placeholder: string;
+}
+
+export default function GoogleMaps({ onLocationSelect, width, height, placeholder }: GoogleMapsProps) {
     const [selectedLocation, setSelectedLocation] = useState <Location>({ lat: null, lng: null });
+    const [showOverlay, setShowOverlay] = useState(true);
 
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
@@ -29,27 +32,53 @@ export default function GoogleMaps() {
     });
 
     const handleMapClick = (event: google.maps.MapMouseEvent) => {
+        setShowOverlay(false);
         if (event.latLng) {
             const lat = event.latLng.lat();
             const lng = event.latLng.lng();
             setSelectedLocation({ lat, lng });
-            console.log(`Selected location: Latitude: ${lat}, Longitude: ${lng}`);
+            onLocationSelect(lat, lng);
         } else {
             console.log("No location selected.");
         }
     };
 
+
     if (!isLoaded) return <div>Loading...</div>;
 
     return (
-        <GoogleMap
-            mapContainerStyle={containerStyle}
-            options={mapOptions}
-            onClick={handleMapClick}
-        >
-            {selectedLocation.lat && selectedLocation.lng && (
-                <Marker position={{ lat: selectedLocation.lat, lng: selectedLocation.lng }} />
+    
+        <div style={{ position: 'relative', height: '100%', width: '100%' }}>
+            {showOverlay && (
+            <div
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Black with opacity
+                    color: 'white',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    zIndex: 2, // Ensure it's above the map
+                }}
+                onClick={() => setShowOverlay(false)} // Hide overlay on click  
+                className = "font-bold text-lg"
+            >
+                {placeholder}
+            </div>
             )}
-        </GoogleMap>
+            <GoogleMap
+                mapContainerStyle={width && height ? { width, height } : { width: "100%", height: "100%" }}
+                options={mapOptions}
+                onClick={handleMapClick}
+            >
+                {selectedLocation.lat && selectedLocation.lng && (
+                    <Marker position={{ lat: selectedLocation.lat, lng: selectedLocation.lng }} />
+                )}
+            </GoogleMap>
+        </div>
     );
 }
