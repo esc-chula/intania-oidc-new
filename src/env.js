defc845bb2018fc1fbbbb4b58c7cefbe95aad120
ebonian <52095091+ebonian@users.noreply.github.com>
2025-02-01 16:38:33 +0700

    fix: hydra env

--- src/env.js
+++ src/env.js
@@ -10,6 +10,7 @@ export const env = createEnv({
         NODE_ENV: z
             .enum(["development", "test", "production"])
             .default("development"),
+        HYDRA_ADMIN_URL: z.string(),
         ALLOW_REDIRECT_URLS: z.string(),
     },
 
@@ -28,6 +29,7 @@ export const env = createEnv({
      */
     runtimeEnv: {
         NODE_ENV: process.env.NODE_ENV || "development",
+        HYDRA_ADMIN_URL: process.env.HYDRA_ADMIN_URL,
         ALLOW_REDIRECT_URLS: process.env.ALLOW_REDIRECT_URLS,
         NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
             process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
